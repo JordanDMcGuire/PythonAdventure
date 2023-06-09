4b9b7f1a14cb64e30cf3fc26974be8f6a185a6c1
Jordan McGuire <jordandmcguire@gmail.com>
2023-06-09 10:45:33 -0600

    Draw Player/Rocks and Add Collisions
    Worked on updating the level and player classes to display a player on the screen and some rocks as obstacles. Each were given collision rectangles and the player class was updated to detect collisions with the rocks and prevent the player from moving through them.

--- code/level.py
+++ code/level.py
@@ -10,7 +10,7 @@ class level:
         self.display_surface = pygame.display.get_surface()
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
         # sprite setup
         self.create_map()
@@ -21,8 +21,11 @@ class level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    Tile((x,y),[self.visible_sprites])
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw the game
-        pass
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
